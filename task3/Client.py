b7363b37eb2607af10c8821002b1e28b13c34b15
M.Beck <mackbeckgames23@gmail.com>
2024-11-27 11:15:26 -0700

    finished client

--- task3/Client.py
+++ task3/Client.py
@@ -1,4 +1,23 @@
+from Director import Director
+from CarBuilder import CarBuilder
+
 director = Director()
 builder = CarBuilder()
-director.make_sports_car(builder)
-car = builder.get_result()
+car = director.make_sports_car(builder)
+
+print("Sports Car")
+print(f"Seats: {car.seats}")
+print(f"Engine: {car.engine}")
+print(f"Trip Computer: {car.trip_computer}")
+print(f"GPS: {car.gps}")
+
+
+
+print("\n")
+print("SUV")
+car = director.make_SUV(builder)
+
+print(f"Seats: {car.seats}")
+print(f"Engine: {car.engine}")
+print(f"Trip Computer: {car.trip_computer}")
+print(f"GPS: {car.gps}")
--- task3/Director.py
+++ task3/Director.py
@@ -3,18 +3,16 @@ from Car import Car
 
 class Director:
     
-    def make_SUV(self) -> Car:
-        builder = CarBuilder()
+    def make_SUV(self, builder : CarBuilder) -> Car:
         builder.setSeats(4)
         builder.setEngine("V8")
         builder.setTripComputer()
         builder.setGps()
-        return builder.getCar()
+        return builder.getResult()
 
-    def make_sports_car(self) -> Car:
-        builder = CarBuilder()
+    def make_sports_car(self, builder : CarBuilder) -> Car:
         builder.setSeats(2)
         builder.setEngine("V12")
         builder.setTripComputer()
         builder.setGps()
-        return builder.getCar()
+        return builder.getResult()
