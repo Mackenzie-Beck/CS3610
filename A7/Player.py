e9883b0ae9f15a6f1fe833ab79fcd63a2bdc4498
Owenyx <ow321rowney@gmail.com>
2024-12-10 08:43:54 -0700

    final

--- A7/Player.py
+++ A7/Player.py
@@ -3,13 +3,13 @@ from Bullet import Bullet
 class Player:
 
     def __init__(self):
-        self.bullet = Bullet()
-        self.upgrades = []
+        self.__bullet = Bullet()
+        self.__upgrades = []
 
     def shoot(self):
-        for upgrade in self.upgrades:
-            upgrade.apply_upgrade(self.bullet)
-        print(f"Bullet shot with damage {self.bullet.damage}, speed {self.bullet.speed}, and pierce {self.bullet.pierce}")
+        for upgrade in self.__upgrades:
+            upgrade.apply_upgrade(self.__bullet)
+        print(f"Bullet shot with damage {self.__bullet.damage}, speed {self.__bullet.speed}, and pierce {self.__bullet.pierce}")
 
     def add_upgrade(self, upgrade):
-        self.upgrades.append(upgrade)
+        self.__upgrades.append(upgrade)
